06975bc234333f03aad9e8990383654e30518960
Dellavrite <delavrite@gmail.com>
2022-12-23 11:17:39 +0300

    auto

--- MozonApp/views.py
+++ MozonApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+from django.views import View
 
 # Create your views here.
+
+class ProductListView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'productlist.html', 
+                      context={'products': Product.objects.all()})
